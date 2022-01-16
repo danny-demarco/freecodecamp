385fe28b22262f658b5553ba12345690155fd1e6
Metamorphor <metamorphosism@protonmail.com>
2022-01-16 18:57:49 +0700

    Pre test version

--- shape_calculator.py
+++ shape_calculator.py
@@ -3,6 +3,10 @@ class Rectangle:
     self.width = width
     self.height = height
   
+  def __str__(self):
+      square = f"Rectangle(width={self.width}, height={self.height})"
+      return square
+  
   def set_width(self, new_width):
     self.width = new_width
     return
@@ -23,14 +27,31 @@ class Rectangle:
   def get_picture(self):
     '''prints the shape as a bunch of *'''
     picture = ""
-    line = "*" * self.width + "\n"
-    picture += line * self.height
-    print(picture)
+    if self.width > 50 or self.height > 50:
+      picture += "too big for picture"
+    else:
+      line = "*" * self.width + "\n"
+      picture += line * self.height
+    return picture
 
   def get_amount_inside(self, shape):
     '''Calculates times the given shape could fit inside this objects shape'''
-    pass
+    no_in_width = int(self.width/shape.width)
+    no_in_height = int(self.height/shape.height)
+    return max(no_in_height, no_in_width)
 
 
 class Square(Rectangle):
-  pass
+  
+  def __init__(self, sides):
+    self.height = sides
+    self.width = sides
+  
+  def __str__(self):
+    square = f"Square(side={self.width})"
+    return square
+
+  def set_side(self, new_sides):
+    self.width = new_sides
+    self.height = new_sides
+    return
