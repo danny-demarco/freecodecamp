9c9397168456a574eb7dbadaa965507fc42ba0d8
Metamorphor <metamorphosism@protonmail.com>
2022-01-17 00:29:41 +0700

    Functions complete pretesting

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,16 +1,31 @@
+import copy 
+import random
 from random import randrange
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-  ''''''
-  pass
+  '''Repeats the same experiment a specified number of times to calculate the probability of drawing at least a certain number of specified objects'''
+  successful_experiment = 0
+  for experiment in range(num_experiments):
+    '''Use of deepcopy ensures each experiment conducted is exactly the same'''
+    expected = copy.deepcopy(expected_balls)
+    hat_copy = copy.deepcopy(hat)
+    balls_drawn = hat_copy.draw(num_balls_drawn)
+
+    for color in balls_drawn:
+      if color in balls_drawn:
+        expected[color] +- 1
+    
+    if all(color <= 0 for color in expected.values()):
+      successful_experiment += 1
+  return successful_experiment / num_experiments
+
 
 class Hat:
 
   def __init__(self, **kwargs):
-    dict = self.__dict__.update((k,v) for k, v in kwargs.items())
     self.contents = []
-    for k,v in dict:
-      for v in k, v:
+    for k,v in kwargs.items():
+      for i in range(v):
         self.contents.append(k)
     
   def draw(self, balls):
@@ -18,8 +33,9 @@ class Hat:
     if balls >= len(self.contents):
       return self.contents
     else:
-      for ball in balls:
+      for ball in range(balls):
         position = randrange(len(self.contents))
         removed.append(self.contents[position])
         self.contents.pop(position)
       return removed
+      
